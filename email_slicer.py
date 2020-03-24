7bc101b9d1eba1e9a670232549de67c9ecc47666
Vinicius Souza <vinicius.fernando.souza@gmail.com>
2020-03-23 23:56:11 -0300

    add email_slicer

---
+++ email_slicer.py
@@ -0,0 +1,11 @@
+def emailSlicer(email):    
+    atSign = email.find('@')
+    dotCom = email.find('.com')
+    user = email[0:atSign].replace('.', ' ')
+    domain = email[atSign+1:dotCom]
+    print("Username: %s" %(user).title())
+    print("Domain: %s" %(domain).capitalize())
+    
+email = input("Digite seu e-mail: ")
+
+emailSlicer(email)
