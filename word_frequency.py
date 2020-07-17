35c66b2178b5359a656026611743cc96ed8aabcd
Vinícius Souza <vinicius.fernando.souza@gmail.com>
2020-07-16 23:12:03 -0300

    update module

--- word_frequency.py
+++ word_frequency.py
@@ -1,18 +1,20 @@
 import spacy
 nlp = spacy.load("pt_core_news_sm")
 
-
-text = input("Your text: ")
-text = nlp(text)
-
-token_list = [token.text for token in text if not token.is_punct]
-unique_list = {}
+def word_frequency(text):
+    text = nlp(text)
+    token_list = [token.text for token in text if not token.is_punct]
+    unique_list = {}
 
 
-for item in token_list:
-    if item not in unique_list:
-        unique_list.update({item: 1})
-    else:
-        unique_list[item] += 1
+    for item in token_list:
+        if item not in unique_list:
+            unique_list.update({item: 1})
+        else:
+            unique_list[item] += 1
 
-print(unique_list)
+    return unique_list
+
+if __name__ == "__main__":
+    text = input("Your text: ")
+    print(word_frequency(text))
