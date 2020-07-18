d11730b27abff8f8a60dc06ccec6ee8db276aff7
Vinícius Souza <vinicius.fernando.souza@gmail.com>
2020-07-17 22:08:40 -0300

    add calc

---
+++ calc/main.py
@@ -0,0 +1,14 @@
+from calc_factories import make_root, make_display,\
+     make_label, make_buttons
+from calc_class import Calculator
+
+def main():
+    root = make_root()
+    display = make_display(root)
+    label = make_label(root)
+    buttons = make_buttons(root)
+    calc = Calculator(root, label, display, buttons)
+    calc.start()
+
+if __name__ == '__main__':
+    main()
